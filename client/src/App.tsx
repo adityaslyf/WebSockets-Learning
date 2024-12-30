d2b122579d5f8cf84f30af50db3e73f88b897de1
adityaslyf <aditya.varshneymail@gmail.com>
2024-12-31 03:04:22 +0530

    Created a server and client send message using websockets

---
+++ client/src/App.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+const App = () => {
+
+  const [socket, setSocket] = useState<null | WebSocket>(null);
+  const [latestmessage, setLatestMessage] = useState<string>('');
+
+
+  useEffect(() => {
+    const socket = new WebSocket('ws://localhost:8080');
+    socket.onopen = () => {
+      console.log('connected');
+      setSocket(socket);
+    }
+    socket.onmessage = (message) => {
+      setLatestMessage(message.data);
+      console.log('message', message.data);
+    }
+
+  },[]);
+
+  if (!socket) {
+    return <div>loading...</div>
+  };
+
+
+  return (
+
+    <div>
+      <input></input>
+      <button onClick={
+        () => socket.send('hello world')}>Send message</button>
+      {latestmessage}
+    </div>
+  )
+}
+
+export default App
